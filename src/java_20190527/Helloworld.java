6eebdb86bae61383d9c2c4c5879e872f9c3e3642
mmikangs <katey0923@gmail.com>
2019-05-27 17:59:47 +0900

    update_2

--- src/java_20190527/Helloworld.java
+++ src/java_20190527/Helloworld.java
@@ -7,5 +7,18 @@ public class Helloworld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		
+		
+		long implement = 1000000000l;
+		
+		double a = 45.56;
+		
+		float n = 45.56f;
+		
+		System.out.println(a);
+		System.out.println(n);
+		
+		
+		
+	
 	}
 }
